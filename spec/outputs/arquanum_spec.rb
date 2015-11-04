515ad3c9d8a6e2ccfe3ad3de4434074718055d0e
Hüseyin Babal <hbabal@ebay.com>
2015-11-04 16:20:49 +0200

    unit tests added for initialization phase

--- spec/outputs/arquanum_spec.rb
+++ spec/outputs/arquanum_spec.rb
@@ -5,8 +5,8 @@ require 'logstash/outputs/arquanum'
 describe 'outputs/arquanum' do
   let(:config) {
     {
-        'token' => 'very_secret_token_12345',
-        'app_id' => 'app_id_12345'
+        'app_id' => 'app_id_12345',
+        'token' => 'very_secret_token_12345'
     }
   }
 
@@ -29,7 +29,33 @@ describe 'outputs/arquanum' do
   end
 
   context 'when initializing' do
+    it 'should register' do
+      expect { ao.register }.to_not raise_error
+    end
+
     it 'should set default api_url if not set' do
+      expect(ao.api_url).to eq('https://api.arquanum.com/logs')
+    end
+
+    it 'should set default api_version if not set' do
+      expect(ao.api_version).to eq('1.0')
+    end
+
+    it 'should set default tag if not set' do
+      expect(ao.tag).to eq('logstash')
+    end
+
+    it 'should set app_id correctly' do
+      expect(ao.app_id).to eq('app_id_12345')
+    end
+
+    it 'should set token correctly' do
+      expect(ao.token).to eq('very_secret_token_12345')
+    end
+  end
+
+  context 'when sending messages' do
+    it 'should send event' do
       allow(ao).to receive(:send_event).with(event.to_json)
       ao.receive(event)
     end
